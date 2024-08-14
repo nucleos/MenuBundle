7e9106d3c464fc313c02af0a8b75ac202a8d0b3a
core23 <mail@core23.de>
2024-08-14 17:43:14 +0200

    Fix symfony deprecation

--- src/DependencyInjection/NucleosMenuExtension.php
+++ src/DependencyInjection/NucleosMenuExtension.php
@@ -13,8 +13,8 @@ namespace Nucleos\MenuBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class NucleosMenuExtension extends Extension
 {
